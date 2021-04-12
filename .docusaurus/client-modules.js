37765c06e62e0561e87d1a0a2762adc6d14d6317
Krishna Satya <vadapallikrishna@gmail.com>
2021-04-12 14:09:10 +0530

    initial

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/krishna/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/krishna/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/krishna/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/home/krishna/docs/node_modules/bootstrap/dist/css/bootstrap.min.css"),
-];
